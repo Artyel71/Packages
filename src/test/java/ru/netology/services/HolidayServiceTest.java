549de4831f58651670b91165cd81620009a355e1
Artyel <artyel@icloud.com>
2023-03-07 14:54:55 +0000

    Calculate added

---
+++ src/test/java/ru/netology/services/HolidayServiceTest.java
@@ -0,0 +1,18 @@
+package ru.netology.services;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+//import ru.netology.services.HolidayService;
+
+public class HolidayServiceTest {
+    @Test
+    public void testHolidayMonth(){
+        HolidayService service = new HolidayService();
+
+        int expected = 2;
+        int actual = service.calculate(100_000, 60_000);
+        Assertions.assertEquals(expected, actual);
+
+    }
+}
